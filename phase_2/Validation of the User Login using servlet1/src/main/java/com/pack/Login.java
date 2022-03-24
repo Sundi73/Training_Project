8429c3022865460ff07909b699582fdd7725fed3
Ashutosh <ashutoshtiwari.cs@gmail.com>
2022-03-24 11:54:51 +0530

    Update in phase2

---
+++ phase_2/Validation of the User Login using servlet1/src/main/java/com/pack/Login.java
@@ -0,0 +1,66 @@
+package com.pack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		
+		String uname = request.getParameter("uname");
+		String upass = request.getParameter("userpassword");
+		
+	//out.println(uname);
+	//out.println(upass);
+		
+		if(uname.equals("admin") && upass.equals("admin")) {
+			HttpSession session= request.getSession();
+			session.setAttribute("uname", uname);			
+			
+			 response.sendRedirect("Dashboardpage");  
+		}else {
+			
+			response.sendRedirect("Invaliddetail");
+		}
+		
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
