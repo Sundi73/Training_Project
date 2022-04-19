06b89aaf8b94f3bcdcf4359e6eda30babf591a6e
Ashutosh <ashutoshtiwari.cs@gmail.com>
2022-04-19 17:36:39 +0530

    added file in phase 3 main project

--- Phase_3/sportyshoes/src/main/java/com/project/sportyshoes/entity/Categories.java
+++ Phase_3/sportyshoes/src/main/java/com/project/sportyshoes/entity/Categories.java
@@ -36,46 +36,5 @@ public Categories(long id, String categoryName) {
 	public void setCategoryName(String categoryName) {
 		this.categoryName = categoryName;
 	}
-	/*public String women;
-	public String men;
-	public String kids;
-	public Categories() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Categories(long id, String women, String men, String kids) {
-		super();
-		this.id = id;
-		this.women = women;
-		this.men = men;
-		this.kids = kids;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getWomen() {
-		return women;
-	}
-	public void setWomen(String women) {
-		this.women = women;
-	}
-public String getMen() {
-		return men;
-	}
-	public void setMen(String men) {
-		this.men = men;
-	}
-	public String getKids() {
-		return kids;
-	}
-	public void setKids(String kids) {
-		this.kids = kids;
-	}
-	@Override
-	public String toString() {
-		return "Categories [id=" + id + ", women=" + women + ", men=" + men + ", kids=" + kids + "]";
-	}*/
+
 }
