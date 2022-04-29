c5e5807ea39d23454b1c1e2a17e5ba4bc7ce89ee
Ashutosh <ashutoshtiwari.cs@gmail.com>
2022-04-29 18:10:58 +0530

    update

--- Phase_3/SpringHibernateDemo/src/main/java/com/ecommerce/service/EproductService.java
+++ Phase_3/SpringHibernateDemo/src/main/java/com/ecommerce/service/EproductService.java
@@ -1,5 +1,21 @@
 package com.ecommerce.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.dao.EproductDAO;
+import com.ecommerce.entity.EproductEntity;
+
+@Service
 public class EproductService {
 
+	@Autowired
+	EproductDAO eproductDAO;
+	
+	public List<EproductEntity> getAllProducts(){
+	return eproductDAO.getAllProduct();
+}
+	
 }
